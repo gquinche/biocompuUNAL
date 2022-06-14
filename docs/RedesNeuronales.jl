f5e88fe65472dadd19a996d2fce0b98eeb9e1023
Gabriel <gabrielquinche@hotmail.com>
2022-06-13 21:14:29 -0500

    docs folder creation

---
+++ docs/RedesNeuronales.jl
@@ -0,0 +1,56 @@
+### A Pluto.jl notebook ###
+# v0.19.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 953b0d4f-c490-4ae5-be67-daeb05af5c63
+md"# Redes neuronales"
+
+# ╔═╡ 8d5b3356-cc80-4fac-92ed-1a9ccab90f5d
+md" ## el problema de reconocer individuos"
+
+# ╔═╡ b90b9ca9-8134-474d-9b46-44dfd2c60014
+md" ## la idea de aprendizaje"
+
+# ╔═╡ 5deb6683-5cf0-4a9b-bb7c-9cb49ef6f34c
+md"## descenso del gradiente "
+
+# ╔═╡ ab3f180c-f848-4756-bcf5-40abf52362d2
+md"## Agregando restricciones a nuestra función de costo"
+
+# ╔═╡ 5130a510-eb7b-11ec-0a10-57175ef8aaf2
+md" ## sistema de compresion red convolucional"
+
+# ╔═╡ 6a8917fe-fb9d-44ad-99f6-a47992c12289
+md"[![EGOB_CQrAR_m4wc5fmKqxjelar_AejoB4SEVADSvwg4.gif](https://s8.gifyu.com/images/EGOB_CQrAR_m4wc5fmKqxjelar_AejoB4SEVADSvwg4.gif)](https://gifyu.com/image/SHIut)"
+
+# ╔═╡ 1864cb21-8eda-48a3-b463-5ae08f5606f6
+md" ## Recomendaciones como usuario y como investigador "
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═953b0d4f-c490-4ae5-be67-daeb05af5c63
+# ╠═8d5b3356-cc80-4fac-92ed-1a9ccab90f5d
+# ╠═b90b9ca9-8134-474d-9b46-44dfd2c60014
+# ╠═5deb6683-5cf0-4a9b-bb7c-9cb49ef6f34c
+# ╠═ab3f180c-f848-4756-bcf5-40abf52362d2
+# ╠═5130a510-eb7b-11ec-0a10-57175ef8aaf2
+# ╠═6a8917fe-fb9d-44ad-99f6-a47992c12289
+# ╠═1864cb21-8eda-48a3-b463-5ae08f5606f6
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
